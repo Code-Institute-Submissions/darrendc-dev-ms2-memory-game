b518d0cf3f3254acbde45f090d5bc6864acd36a3
darrendc-dev <ddalycarolan@hotmail.co.uk>
2021-01-09 20:30:02 +0000

    Added a flip card function and fixed a visual mistake on flipcard class

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,20 @@
     window.onload = function() {
         $('.playingCard').addClass('flipCard');
     };
+
+    // Card Event Listener
+
+    $('.playingCard').click(function(){
+        if (pauseGame) return;
+        $(this).removeClass('flipCard');
+        if(!flippedCard){
+            console.log("First card");
+            flippedCard = true;
+            selected = this;
+            firstFlip = this;
+        } else {
+                console.log("Second card");
+                flippedCard = false;
+                secondFlip = this;
+            };  
+        });
